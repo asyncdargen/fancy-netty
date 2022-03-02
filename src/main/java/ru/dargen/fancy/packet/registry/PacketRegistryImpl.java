417287f63dd26e75a52a93d4650fd43f972e17c2
root <dargencode@mail.ru>
2022-03-02 07:56:27 +0300

    add automatic registretion from classloader | 1.6

--- src/main/java/ru/dargen/fancy/packet/registry/PacketRegistryImpl.java
+++ src/main/java/ru/dargen/fancy/packet/registry/PacketRegistryImpl.java
@@ -1,9 +1,20 @@
 package ru.dargen.fancy.packet.registry;
 
+import com.google.common.reflect.ClassPath;
+import lombok.SneakyThrows;
 import ru.dargen.fancy.packet.Packet;
+import ru.dargen.fancy.util.FancyException;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
 
 public class PacketRegistryImpl implements PacketRegistry {
 
@@ -21,6 +32,50 @@ public class PacketRegistryImpl implements PacketRegistry {
             if (!type.isAnnotationPresent(Packet.Id.class))
                 throw new IllegalArgumentException("packet class not annotated");
             register(type.getAnnotation(Packet.Id.class).value(), type);
+        }
+    }
+
+    public void registerFromCurrentJar() {
+        registerFromCurrentJar(Packet.class);
+    }
+
+    public void registerFromCurrentJar(Class<? extends Packet> clazz) {
+        registerFromClassLoader(Thread.currentThread().getContextClassLoader(), clazz);
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    public void registerFromClassLoader(ClassLoader loader, Class<? extends Packet> type) {
+        try {
+            for (ClassPath.ClassInfo info : ClassPath.from(loader).getAllClasses()) {
+                try {
+                    Class<?> clazz = info.load();
+                    if (type.isAssignableFrom(clazz) && clazz.isAnnotationPresent(Packet.Id.class)) {
+                        register(clazz.getAnnotation(Packet.Id.class).value(), (Class<? extends Packet>) clazz);
+                    }
+                } catch (Throwable __) {}
+            }
+//            //TODO: Maybe caught exception in javac
+//            File currentJarFile = new File(PacketRegistryImpl.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+//            System.out.println(currentJarFile);
+//            JarFile jar = new JarFile(currentJarFile);
+//            Enumeration<JarEntry> enumeration = jar.entries();
+//            JarEntry entry;
+//            while (enumeration.hasMoreElements() && (entry = enumeration.nextElement()) != null) {
+//                if (!entry.getName().endsWith(".class"))
+//                    continue;
+//
+//                String path = entry.getName()
+//                        .replace("/", ".")
+//                        .replace(".class", "");
+//                try {
+//                    Class<?> clazz = loader.loadClass(path);
+//                    if (Packet.class.isAssignableFrom(clazz) && clazz.isAnnotationPresent(Packet.Id.class)) {
+//                        register(clazz.getAnnotation(Packet.Id.class).value(), (Class<? extends Packet>) clazz);
+//                    }
+//                } catch (Throwable e) {}
+//            }
+        } catch (Throwable e) {
+            throw new FancyException("Error while search and register packets: " + e);
         }
     }
 
